f216317963e6e16d6a3d41e94fe7ce1e69f30876
VYuan <vyuanchao@hotmail.com>
2025-04-28 22:49:23 +0930

    Add boot script

---
+++ petalinux/kria/boot_scripts/boot.cmd
@@ -0,0 +1,8 @@
+setenv bootargs console=ttyPS0,115200 root=/dev/mmcblk0p2 rw rootwait clk_ignore_unused
+
+fatload mmc 1 0x10000000 system.bit
+fpga load 0 0x10000000 ${filesize}
+
+fatload mmc 1 0x20000000 image.ub
+fatload mmc 1 0x18000000 system.dtb
+bootm 0x20000000 - 0x18000000
