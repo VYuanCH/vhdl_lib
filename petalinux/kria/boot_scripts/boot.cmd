5be8e520abfe6ca3b4661fb58c25ec55e625df40
VYuan <vyuanchao@hotmail.com>
2025-05-04 17:04:51 +0930

    update device tree and bootscript for dma

--- petalinux/kria/boot_scripts/boot.cmd
+++ petalinux/kria/boot_scripts/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttyPS0,115200 root=/dev/mmcblk0p2 rw rootwait clk_ignore_unused
+setenv bootargs console=ttyPS0,115200 root=/dev/mmcblk0p2 rw rootwait clk_ignore_unused mem=1532M memmap=512M$0x60000000
 
 fatload mmc 1 0x10000000 system.bit
 fpga load 0 0x10000000 ${filesize}
