58615d8dc0f2dc02fb54dcb96c8684160b4db04e
VYuan <vyuanchao@hotmail.com>
2025-05-16 22:47:12 +0930

    Add array type conversion

--- vhdl/pkg/array_types.vhd
+++ vhdl/pkg/array_types.vhd
@@ -8,30 +8,73 @@ package array_types is
     type array_signed_t is array (natural range <>) of signed;
     type array_slv_t is array (natural range <>) of std_logic_vector;
 
-    function to_array_slv(unsigned_array : array_unsigned_t) return array_slv_t;
-    function to_array_slv(signed_array : array_signed_t) return array_slv_t;
+    function to_array_slv(input_array : array_unsigned_t) return array_slv_t;
+    function to_array_slv(input_array : array_signed_t) return array_slv_t;
+
+    function to_array_unsigned(input_array : array_slv_t) return array_unsigned_t;
+    function to_array_unsigned(input_array : array_signed_t) return array_unsigned_t;
+
+    function to_array_signed(input_array : array_slv_t) return array_signed_t;
+    function to_array_signed(input_array : array_unsigned_t) return array_signed_t;
 end package;
 
 
 
 package body array_types is
-    function to_array_slv(unsigned_array : array_unsigned_t) return array_slv_t is
-      variable slv_array  : array_slv_t(unsigned_array'range)(unsigned_array(unsigned_array'left)'range);
+
+    function to_array_slv(input_array : array_unsigned_t) return array_slv_t is
+      variable result_array  : array_slv_t(input_array'range)(input_array(input_array'left)'range);
     begin
-        for i in unsigned_array'range loop
-            slv_array(i) := std_logic_vector(unsigned_array(i));
+        for i in input_array'range loop
+            result_array(i) := std_logic_vector(input_array(i));
         end loop;
-        return slv_array;
+        return result_array;
+    end;
+    function to_array_slv(input_array : array_signed_t) return array_slv_t is
+      variable result_array  : array_slv_t(input_array'range)(input_array(input_array'left)'range);
+    begin
+        for i in input_array'range loop
+            result_array(i) := std_logic_vector(input_array(i));
+        end loop;
+        return result_array;
     end;
 
-    function to_array_slv(signed_array : array_signed_t) return array_slv_t is
-      variable slv_array  : array_slv_t(signed_array'range)(signed_array'left'high downto 0);
-    begin
-        for i in signed_array'range loop
-            slv_array(i) := std_logic_vector(signed_array(i));
+    
+    function to_array_unsigned(input_array : array_slv_t) return array_unsigned_t is
+      variable result_array  : array_unsigned_t(input_array'range)(input_array(input_array'left)'range);
+      begin
+        for i in input_array'range loop
+            result_array(i) := unsigned(input_array(i));
         end loop;
-        return slv_array;
+        return result_array;
+      end;
+    function to_array_unsigned(input_array : array_signed_t) return array_unsigned_t is
+      variable result_array  : array_unsigned_t(input_array'range)(input_array(input_array'left)'range);
+    begin
+        for i in input_array'range loop
+            result_array(i) := unsigned(input_array(i));
+        end loop;
+        return result_array;
     end;
+  
+
+    function to_array_signed(input_array : array_slv_t) return array_signed_t is
+        variable result_array  : array_signed_t(input_array'range)(input_array(input_array'left)'range);
+        begin
+          for i in input_array'range loop
+              result_array(i) := signed(input_array(i));
+          end loop;
+          return result_array;
+        end;
+      function to_array_signed(input_array : array_unsigned_t) return array_signed_t is
+        variable result_array  : array_signed_t(input_array'range)(input_array(input_array'left)'range);
+      begin
+          for i in input_array'range loop
+              result_array(i) := signed(input_array(i));
+          end loop;
+          return result_array;
+      end;
+      
   end package body;
   
   
